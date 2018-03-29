15f9d397bab9e3babb40545b2ce8a5ffc9831d44
daved09 <daluba@live.de>
2018-03-30 01:27:10 +0200

    gui is almost finished

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -2,10 +2,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import ui_tools.MessageBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,6 +30,20 @@ public class MainWindowController implements Initializable {
 
     private Stage getStageFromWindow(){
         return (Stage) noteList.getScene().getWindow();
+    }
+
+    public void newNote(ActionEvent event){
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("gui/NoteWindow.fxml"));
+        try {
+            Parent root = loader.load();
+            stage.setTitle("Note");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        } catch (IOException e) {
+            MessageBox.showError(e.getMessage());
+        }
+
     }
 
 }
--- src/main/java/controller/NoteWindowController.java
+++ src/main/java/controller/NoteWindowController.java
@@ -1,16 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import ui_tools.MessageBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NoteWindowController implements Initializable {
 
+    @FXML
+    private TextField txtTitle;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
+    public void keyPress(KeyEvent event){
+        if(event.isControlDown() && event.getCode() == KeyCode.S){
+            MessageBox.show("Save");
+        }
+    }
 
 }
